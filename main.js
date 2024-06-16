9cd118e58177512e5413082152afb11d1516238b
adrianoelguapo <ex8zari@gmail.com>
2024-06-16 17:13:06 +0200

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $.getJSON("https://random-data-api.com/api/users/random_user?size=8",function (datosJSON){
+        let usuarios = datosJSON;
+
+        for (let cadaUsuario of usuarios){
+            $(".tarjetas").append(`<div class = "tarjeta"> <div class = "fototarjeta"> <img src = "${cadaUsuario.avatar}" height = "115px"> </div> <div class = "contenidotarjeta"> <p class = "nombretarjeta"> ${cadaUsuario.first_name} ${cadaUsuario.last_name} </p> <p class = "emailtarjeta"> ${cadaUsuario.email} </p> <p class = "puestotarjeta"> ${cadaUsuario.employment.title} </p> <p class = "ubitarjeta"> ${cadaUsuario.address.city}, ${cadaUsuario.address.state} </p> </div> </div>`)
+        }
+    })
+})
